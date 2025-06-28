dea3467ea37792bc107ac0e52128164f5ba82caf
BoissonMathis <math.boiss@laposte.net>
2025-06-28 17:56:23 +0200

    add create user dto struct #9

--- internal/auth/usermodel.go
+++ internal/auth/usermodel.go
@@ -1,8 +1,6 @@
 package auth
 
 import (
-	"time"
-
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -12,6 +10,13 @@ type User struct {
 	Password  string               `bson:"password" json:"password"`
 	CrmID     string               `bson:"crm_id" json:"crm_id"`
 	RoleIDs   []primitive.ObjectID `bson:"role_ids" json:"role_ids"`
-	CreatedAt time.Time            `bson:"created_at" json:"created_at"`
-	UpdatedAt time.Time            `bson:"updated_at" json:"updated_at"`
+	CreatedAt primitive.DateTime   `bson:"created_at" json:"created_at"`
+	UpdatedAt primitive.DateTime   `bson:"updated_at" json:"updated_at"`
+}
+
+type UserCreateDto struct {
+	Email    string 				`json:"email" binding:"required,email"`
+	Password string 				`json:"password" binding:"required,min=6"`
+	Username string 				`json:"username" binding:"required"`
+	RoleIDs  []primitive.ObjectID 	`bson:"role_ids" json:"role_ids"`
 }
