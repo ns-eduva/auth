469ec1917187c86ede7b37dcb24babf080747449
BoissonMathis <math.boiss@laposte.net>
2025-06-28 17:58:50 +0200

    refacto auth model #9

--- internal/auth/authmodel.go
+++ internal/auth/authmodel.go
@@ -4,7 +4,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type User struct {
+type Auth struct {
 	ID        primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
 	Email     string               `bson:"email" json:"email"`
 	Password  string               `bson:"password" json:"password"`
@@ -14,9 +14,8 @@ type User struct {
 	UpdatedAt primitive.DateTime   `bson:"updated_at" json:"updated_at"`
 }
 
-type UserCreateDto struct {
+type AuthCreateDto struct {
 	Email    string 				`json:"email" binding:"required,email"`
 	Password string 				`json:"password" binding:"required,min=6"`
-	Username string 				`json:"username" binding:"required"`
 	RoleIDs  []primitive.ObjectID 	`bson:"role_ids" json:"role_ids"`
 }
