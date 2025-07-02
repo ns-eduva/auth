cd46d15a9fbae77a01fdf9ee9d3e681ee230667e
BoissonMathis <math.boiss@laposte.net>
2025-07-02 15:27:57 +0200

    add func findbyemail #9

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,6 +1,11 @@
 package auth
 
 import (
+	"context"
+	"errors"
+
+	"github.com/nsevenpack/logger/v2/logger"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -9,11 +14,25 @@ type authService struct {
 }
 
 type AuthServiceInterface interface {
-	// Create(ctx context.Context, auth *Auth) error
+	FindByEmail(ctx context.Context, email string) (*Auth, error)
 }
 
 func NewAuthService(db *mongo.Database) AuthServiceInterface {
 	return &authService{
 		collection: db.Collection("auth"),
 	}
+}
+
+func (s *authService) FindByEmail(ctx context.Context, email string) (*Auth, error) {
+	var auth Auth
+	err := s.collection.FindOne(ctx, bson.M{"email": email}).Decode(&auth)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			logger.Wf("Erreur mongo no document: %v", err)
+			return nil, nil
+		}
+		logger.Ef("Erreur à la recuperation du auth: %v", err)
+		return nil, err
+	}
+	return &auth, nil
 }
